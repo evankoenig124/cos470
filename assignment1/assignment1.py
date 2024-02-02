66e87cdb89fa7f3928df4282f700b5efa60ba80b
“Evan <evan.koenig@maine.edu>
2024-02-02 18:27:30 -0500

    COMMENTS

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -3,17 +3,7 @@ from bs4 import BeautifulSoup
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
-
-godFather_link = "https://www.dailyscript.com/scripts/The_Godfather.html"
-godFather2_link = "https://www.dailyscript.com/scripts/godfather2.html"
-
-html_gf1 = requests.get(godFather_link)
-html_gf2 = requests.get(godFather2_link)
-
-soup_gf1 = BeautifulSoup(html_gf1.text, features='html.parser')
-soup_gf2 = BeautifulSoup(html_gf2.text, features='html.parser')
-
-def extract_lines(character, soup):
+def scrape_lines(character, soup):
     script = soup.find("pre")
     script = script.get_text().splitlines()
     newscript = ''
@@ -28,24 +18,45 @@ def extract_lines(character, soup):
         line += 1
     return newscript
 
-michael_lines_gf1 = extract_lines("MICHAEL", soup_gf1)
-michael_lines_gf2 = extract_lines("MICHAEL", soup_gf2)
+def wordcloud_plot(lines1, lines2):
+    wordcloud1 = WordCloud(width=600, height=200,background_color='white').generate(lines1)
+    plt.figure(figsize=(10, 6))
+    plt.imshow(wordcloud1)
+    plt.axis('off')
+    plt.show()
 
-open("Godfather1.txt", "w", encoding="utf-8").write(michael_lines_gf1)
-open("Godfather2.txt", "w", encoding="utf-8").write(michael_lines_gf2)
+    wordcloud2 = WordCloud(width=600, height=200,background_color='white').generate(lines2)
+    plt.figure(figsize=(10, 6))
+    plt.imshow(wordcloud2)
+    plt.axis('off')
+    plt.show()
 
-michael_lines_gf1 = michael_lines_gf1.replace('MICHAEL', '')
-michael_lines_gf2 = michael_lines_gf2.replace('MICHAEL', '')
+def main():
+    godFather_link = "https://www.dailyscript.com/scripts/The_Godfather.html"
+    godFather2_link = "https://www.dailyscript.com/scripts/godfather2.html"
+    #LINKS FOR SCRAPING
 
-wordcloud1 = WordCloud(width=600, height=200,background_color='white').generate(michael_lines_gf1)
-plt.figure(figsize=(5, 3))
-plt.imshow(wordcloud1)
-plt.axis('off')
-plt.show()
+    html_gf1 = requests.get(godFather_link)
+    html_gf2 = requests.get(godFather2_link)
+    #REQUESTS LINKS, SAVES IN VAR
 
-wordcloud2 = WordCloud(width=600, height=200,background_color='white').generate(michael_lines_gf2)
-plt.figure(figsize=(5, 3))
-plt.imshow(wordcloud2)
-plt.axis('off')
-plt.show()
+    soup_gf1 = BeautifulSoup(html_gf1.text, features='html.parser')
+    soup_gf2 = BeautifulSoup(html_gf2.text, features='html.parser')
+    #TURN INTO BEAUTFIULSOUP OBJECTS
+
+    michael_lines_gf1 = scrape_lines("MICHAEL", soup_gf1)
+    michael_lines_gf2 = scrape_lines("MICHAEL", soup_gf2)
+    #SCRAPE ALL LINES OF CHARACTER "MICHAEL"
+
+    open("Godfather1.txt", "w", encoding="utf-8").write(michael_lines_gf1)
+    open("Godfather2.txt", "w", encoding="utf-8").write(michael_lines_gf2)
+    #WRITES SCRAPED LINES INTO TEXT FILES
+    
+    michael_lines_gf1 = michael_lines_gf1.replace('MICHAEL', '')
+    michael_lines_gf2 = michael_lines_gf2.replace('MICHAEL', '')
+    #PREPARATION FOR PLOT, GETTING RID OF MICHAEL
+
+    wordcloud_plot(michael_lines_gf1,michael_lines_gf2)
+    #WORLDCLOUD PLOT
+main()
 
