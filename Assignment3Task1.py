982b3dbd1a507e663dc15e2a71be09f3a5320bc4
“Evan <evan.koenig@maine.edu>
2024-03-12 04:38:04 -0400

    divine intervention

--- Assignment3Task1.py
+++ Assignment3Task1.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import csv
+import re
 
 def scrapesongs(path):
     with open('trainingdata.csv', 'w', newline='') as csvfile:
@@ -15,10 +16,16 @@ def scrapesongs(path):
                     html = requests.get(f'https://www.popvortex.com/music/charts/top-{genre.lower()}-songs.php')
                 soup = BeautifulSoup(html.text, features='html.parser')
                 song_spans = soup.find_all('cite', class_='title')
-                for span in song_spans:
-                    song_name = span.text.strip()
+                artist_spans = soup.find_all('em',class_='artist')
+                for i in range(len(song_spans)):
+                    song_name = re.sub(r'[^a-zA-Z]', '-', song_spans[i].text)
+                    artist_name = re.sub(r'[^a-zA-Z]', '-', artist_spans[i].text)
+                    lyricshtml = requests.get(f'https://genius.com/{artist_name}-{song_name}-lyrics')
+                    lyricsoup = BeautifulSoup(lyricshtml.text, features='html.parser')
+                    lyrics = lyricsoup.find('div', class_='Lyrics__Container-sc-1ynbvzw-1 kUgSbL')
                     if song_name not in path:
-                        csv_writer.writerow([song_name, genre])
+                        if lyrics:
+                            csv_writer.writerow([song_name, artist_name,genre,lyrics.get_text(separator=' ')])
 
 def main():
     testsongspath = "/Users/evankoenig/Downloads/Test Songs"
